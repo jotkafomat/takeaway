cf905518c649997b14f3d140c7830ce0de4c4463
jotkafomat <k-j@xl.wp.pl>
2021-07-27 12:40:54 +0100

    Initial Commit

---
+++ takeaway/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  takeaway
+//
+//  Created by Krzysztof Jankowski on 27/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ takeaway/takeawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  takeawayApp.swift
+//  takeaway
+//
+//  Created by Krzysztof Jankowski on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct takeawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
